1df650e7d8e74011f5ff6fffd65f765858b4be1a
Egor Shkarin <schkarin.egor@yandex.ru>
2021-12-17 18:52:37 +0300

    Поправил лабы

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,16 +1,12 @@
-from lab_python_oop.rectangle import Rectangle
-from lab_python_oop.circle import Circle
-from lab_python_oop.square import Square
-import arrow
+from lab_oop.rectangle import Rectangle
+from lab_oop.circle import Circle
+from lab_oop.square import Square
 
 
 def main():
-    r = Rectangle("синего", 5, 6)
-    c = Circle("красного", 11)
-    s = Square("желтого", 11)
-
-    utc_time = arrow.utcnow()
-    print('Времечко у нас такое вот (UTC): ', utc_time)
+    r = Rectangle("Синего", 7, 0)
+    c = Circle("Красного", 13)
+    s = Square("Желтого", 14)
     print(r)
     print(c)
     print(s)
